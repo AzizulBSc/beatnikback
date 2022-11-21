483c7301f8cb97af1b0d614964c72e7c00f1b19a
AzizulHoque <azizulh8774@gmail.com>
2022-11-22 00:34:34 +0600

    link updated

--- resources/views/layouts/admin.blade.php
+++ resources/views/layouts/admin.blade.php
@@ -7,8 +7,8 @@
     <meta http-equiv="x-ua-compatible" content="ie=edge">
 
     <title>Admin Panel</title>
-    <link rel="stylesheet" href="{{ secure_asset('admin/plugins/fontawesome-free/css/all.min.css') }}">
-    <link rel="stylesheet" href="{{ secure_asset('admin/css/adminlte.min.css') }}">
+    <link rel="stylesheet" href="{{ asset('admin') }}/plugins/fontawesome-free/css/all.min.css">
+    <link rel="stylesheet" href="{{ asset('admin') }}/css/adminlte.min.css">
     <link href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,400i,700" rel="stylesheet">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/toastr.js/latest/toastr.min.css">
     @yield('style')
@@ -33,7 +33,7 @@
         <aside class="main-sidebar sidebar-dark-primary elevation-4">
             <!-- Brand Logo -->
             <a href="" class="brand-link">
-                <img src="{{ secure_asset('admin/img/AdminLTELogo.png')}}" alt="AdminLTE Logo"
+                <img src="{{ asset('admin') }}/img/AdminLTELogo.png" alt="AdminLTE Logo"
                     class="brand-image img-circle elevation-3" style="opacity: .8">
                 <span class="brand-text font-weight-light">Dashboard</span>
             </a>
@@ -43,7 +43,7 @@
                 <!-- Sidebar user panel (optional) -->
                 <div class="user-panel mt-3 pb-3 mb-3 d-flex">
                     <div class="image">
-                        <img src="{{ secure_asset('admin/img/user2-160x160.jpg') }}" class="img-circle elevation-2"
+                        <img src="{{ asset('admin') }}/img/user2-160x160.jpg" class="img-circle elevation-2"
                             alt="User Image">
                     </div>
                     <div class="info">
@@ -156,12 +156,12 @@
 
     <!-- REQUIRED SCRIPTS -->
     <!-- jQuery -->
-    <script src="{{ secure_asset(('admin/plugins/jquery/jquery.min.js') }}"></script>
+    <script src="{{ asset('admin') }}/plugins/jquery/jquery.min.js"></script>
     <!-- Bootstrap 4 -->
-    <script src="{{secure_asset('admin/plugins/bootstrap/js/bootstrap.bundle.min.js') }}"></script>
+    <script src="{{ asset('admin') }}/plugins/bootstrap/js/bootstrap.bundle.min.js"></script>
     <!-- AdminLTE App -->
-    <script src="{{ secure_asset(('admin/js/adminlte.min.js') }}"></script>
-    <script src="{{ secure_asset(('admin/js/bs-custom-file-input.min.js') }}"></script>
+    <script src="{{ asset('admin') }}/js/adminlte.min.js"></script>
+    <script src="{{ asset('admin') }}/js/bs-custom-file-input.min.js"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/toastr.js/latest/toastr.min.js"></script>
     @yield('script')
     <script>
--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -6,18 +6,18 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="csrf-token" content="{{ csrf_token() }}">
 
-    <title>Beatnik Fronend | Log in</title>
+    <title>Beatnick Tech</title>
     <!-- Tell the browser to be responsive to screen width -->
     <meta name="viewport" content="width=device-width, initial-scale=1">
 
     <!-- Font Awesome -->
-    <link rel="stylesheet" href="{{ secure_asset('admin/plugins/fontawesome-free/css/all.min.css')}}">
+    <link rel="stylesheet" href="{{ asset('admin') }}/plugins/fontawesome-free/css/all.min.css">
     <!-- Ionicons -->
     <link rel="stylesheet" href="https://code.ionicframework.com/ionicons/2.0.1/css/ionicons.min.css">
     <!-- icheck bootstrap -->
-    <link rel="stylesheet" href="{{ secure_asset('admin/plugins/icheck-bootstrap/icheck-bootstrap.min.css')}}">
+    <link rel="stylesheet" href="{{ asset('admin') }}/plugins/icheck-bootstrap/icheck-bootstrap.min.css">
     <!-- Theme style -->
-    <link rel="stylesheet" href="{{ secure_asset('admin/css/adminlte.min.css')}}">
+    <link rel="stylesheet" href="{{ asset('admin') }}/css/adminlte.min.css">
     <!-- Google Font: Source Sans Pro -->
     <link href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,400i,700" rel="stylesheet">
 </head>
@@ -36,11 +36,11 @@
     <!-- /.login-box -->
 
     <!-- jQuery -->
-    <script src="{{secure_asset('admin/plugins/jquery/jquery.min.js')}}"></script>
+    <script src="{{ asset('admin') }}/plugins/jquery/jquery.min.js"></script>
     <!-- Bootstrap 4 -->
-    <script src="{{ secure_asset('admin/plugins/bootstrap/js/bootstrap.bundle.min.js')}}"></script>
+    <script src="{{ asset('admin') }}/plugins/bootstrap/js/bootstrap.bundle.min.js"></script>
     <!-- AdminLTE App -->
-    <script src="{{ secure_asset('admin/js/adminlte.min.js')}}"></script>
+    <script src="{{ asset('admin') }}/js/adminlte.min.js"></script>
 
 </body>
 
