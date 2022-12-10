a8805afe1a992deea0b00f20e1df0fe7ceacde30
Azizul Hoque <44068019+AzizulBSc@users.noreply.github.com>
2022-12-10 19:17:19 +0600

    Update check

--- app/Http/Controllers/Auth/RegisterController.php
+++ app/Http/Controllers/Auth/RegisterController.php
@@ -65,7 +65,6 @@ class RegisterController extends Controller
      */
     protected function create(array $data)
     {
-        return "sorry something wrong";
         $user = User::create([
             'name' => $data['name'],
             'email' => $data['email'],
